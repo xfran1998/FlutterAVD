3be185188ae9804794818f0dbf1eb11074b9c300
Francisco <seafran@alumni.uv.es>
2021-08-17 23:05:35 +0200

    commented Simple version

--- avdRunSimple.cpp
+++ avdRunSimple.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <windows.h>
 
+// Faster harcoded version, click and run
 
 int main()
 {
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	
+	// Change the route and the android device for what ever you want to use
 	system("D: && cd D:\\Programas\\android-sdk\\emulator && emulator -avd Nexus_5_API_30");
 	
 	return 0;
