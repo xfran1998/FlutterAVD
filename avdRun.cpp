1127fa253493868f7e6d378f3fa72f469e0937af
Francisco <seafran@alumni.uv.es>
2021-08-17 21:58:12 +0200

    Refactoring some things and starting to set location by text file insted of hardcoded on the code

--- avdRun.cpp
+++ avdRun.cpp
@@ -3,42 +3,28 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <fstream>
 #include <windows.h>
 
-std::string exec(const char* cmd) {
-    char buffer[128];
-    std::string result = "";
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    try {
-        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-            result += buffer;
-        }
-    } catch (...) {
-        pclose(pipe);
-        throw;
-    }
-    pclose(pipe);
-    return result;
-}
-
+void SetLocation();
+void GetLocation(std::string* cmd);
+std::string Exec(const char* cmd); 
 bool ValidOption(int option, int maxSize);
+void SplitAVD(std::vector<std::string>& avdDevices, std::string output);
 void ShowAvdDevices(std::vector<std::string> avdDevices);
 
 int main()
 {
 	std::vector<std::string> avdDevices;
 	
+	SetLocation();
+	
 	try
 	{
-		std::string output = exec("D: && cd D:\\Programas\\android-sdk\\emulator && emulator -list-avds");
-		int index = output.find('\n');
+		std::string output = Exec("D: && cd D:\\Programas\\android-sdk\\emulator && emulator -list-avds");
 		
-		while(index > 0){
-			avdDevices.push_back(output.substr(0, index));
-			output.erase(0, index+1);
-			index = output.find('\n');
-		}
+		// Spliting all the Android devices for later choose
+		SplitAVD(avdDevices, output);
 		
 		int option = -1;
 		bool missed = false;
@@ -69,14 +55,74 @@ int main()
 	}
 	catch (std::runtime_error e)
 	{
-		std::cout << "Error cmd" << std::endl;
+		std::cout << "Error: " << e.what() << std::endl;
 	}
 	
-	
-	//system("D: && cd D:\\Programas\\android-sdk\\emulator && emulator -list-avds");
-	//emulator -avd Nexus_5_API_30
-	
 	return 0;	
+}
+
+void SetLocation()
+{	
+	// Checking if already exists, so it's setted
+	std::fstream file;
+	file.open("avdSetting.txt");
+	
+	if (file.fail()) // No exists so need to set location for first time
+	{
+		std::ofstream fOut("avdSetting.txt", std::ofstream::out);
+		std::string emulatorRoute;
+		
+		std::cout << "Note: YOU CAN CHANGE this route on ( avdSetting.txt ) if you get it wrong";
+		//std::cout << "in case you want to change it manually you should use \\\\ instead of \\\n\n";
+		
+		std::cout << "What is the route of the emulator?" << std::endl;
+		std::cout << "Example: D:\\Programas\\android-sdk\\emulator" << std::endl;
+		
+		std::cout << "Route: ";
+		getline(std::cin, emulatorRoute);
+		
+		// Debug
+		std::cout << "Route: " << emulatorRoute << std::endl;
+		while(true);
+		
+		// Erasing posible white space on the first char
+		int spaceIndex = emulatorRoute.find(' ');
+		while(spaceIndex != 0)
+		{
+			emulatorRoute.erase(emulatorRoute.begin());
+			spaceIndex = emulatorRoute.find(' ');
+		}
+		
+		fOut << "Route: " << emulatorRoute;
+	}
+}
+
+/*
+*	Function for take the output log of the cmd 
+*
+*	Param in:
+*		const char* cmd: Command used in the cmd to get into emulator folder and get all available Android devices
+*	
+*	Param out:
+*		std::string result: Output of the console log with all available Android Android devices
+*				
+*/
+std::string Exec(const char* cmd) 
+{
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
 }
 
 bool ValidOption(int option, int maxSize)
@@ -93,3 +139,15 @@ void ShowAvdDevices(std::vector<std::string> avdDevices)
 	
 	std::cout << std::endl;
 }
+
+void SplitAVD(std::vector<std::string>& avdDevices, std::string output)
+{
+	int index = output.find('\n');
+	
+	while(index > 0){
+		avdDevices.push_back(output.substr(0, index));
+		output.erase(0, index+1);
+		index = output.find('\n');
+	}	
+}
+
